5a3b48bde1f5052b2f68bb0aa192d276ed108cf7
Mehmet Cetin <cetin@kinematiks.com>
2024-02-26 14:18:29 +0300

    asdg

--- klines/streamer.go
+++ klines/streamer.go
@@ -1,24 +1,81 @@
 package klines
 
 import (
+	"encoding/json"
 	"fmt"
-	"log"
 	"net/url"
+	"strconv"
 	"strings"
 
 	"github.com/gorilla/websocket"
 	"github.com/varga-lp/data/config"
 )
 
+type klineResp struct {
+	K struct {
+		OpenTime       int64  `json:"t"`
+		Open           string `json:"o"`
+		High           string `json:"h"`
+		Low            string `json:"l"`
+		Close          string `json:"c"`
+		Volume         string `json:"v"`
+		TakerBuyVolume string `json:"V"`
+		NumberOfTrades int64  `json:"n"`
+		CloseTime      int64  `json:"T"`
+		IsFinal        bool   `json:"x"`
+		Omit           int64  `json:"L"` // this is to avoid case-insensitive unmarshalling
+	} `json:"k"`
+}
+
+func (kr *klineResp) toKline() (Kline, error) {
+	open, err := strconv.ParseFloat(kr.K.Open, 64)
+	if err != nil {
+		return Kline{}, err
+	}
+	high, err := strconv.ParseFloat(kr.K.High, 64)
+	if err != nil {
+		return Kline{}, err
+	}
+	low, err := strconv.ParseFloat(kr.K.Low, 64)
+	if err != nil {
+		return Kline{}, err
+	}
+	close, err := strconv.ParseFloat(kr.K.Close, 64)
+	if err != nil {
+		return Kline{}, err
+	}
+	volume, err := strconv.ParseFloat(kr.K.Volume, 64)
+	if err != nil {
+		return Kline{}, err
+	}
+	takerBuyVolume, err := strconv.ParseFloat(kr.K.TakerBuyVolume, 64)
+	if err != nil {
+		return Kline{}, err
+	}
+
+	return Kline{
+		OpenTime:       kr.K.OpenTime,
+		Open:           open,
+		High:           high,
+		Low:            low,
+		Close:          close,
+		Volume:         volume,
+		TakerBuyVolume: takerBuyVolume,
+		NumberOfTrades: kr.K.NumberOfTrades,
+		CloseTime:      kr.K.CloseTime,
+		IsFinal:        kr.K.IsFinal,
+	}, nil
+}
+
 type Streamer struct {
 	symbol     string
 	streamName string
-	updateChan chan Kline
-	errorChan  chan error
-	closeChan  chan struct{}
+	updateChan chan<- Kline
+	errorChan  chan<- error
+	closeChan  <-chan struct{}
 }
 
-func NewStreamer(symbol string, updateChan chan Kline, errChan chan error, closeChan chan struct{}) *Streamer {
+func NewStreamer(symbol string, updateChan chan<- Kline, errChan chan<- error, closeChan <-chan struct{}) *Streamer {
 	symL := strings.ToLower(symbol)
 
 	return &Streamer{
@@ -44,12 +101,22 @@ func (s *Streamer) Dial() error {
 				c.Close()
 				return
 			default:
-				mt, message, err := c.ReadMessage()
+				_, message, err := c.ReadMessage()
 				if err != nil {
 					s.errorChan <- err
 					return
 				}
-				log.Println(mt, string(message))
+				var kr klineResp
+				if err := json.Unmarshal(message, &kr); err != nil {
+					s.errorChan <- err
+					return
+				}
+				kline, err := kr.toKline()
+				if err != nil {
+					s.errorChan <- err
+					return
+				}
+				s.updateChan <- kline
 			}
 		}
 	}()
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
@@ -13,13 +14,25 @@ func main() {
 	shutdownCh := make(chan os.Signal, 1)
 	signal.Notify(shutdownCh, syscall.SIGINT, syscall.SIGTERM)
 
+	klineChan := make(chan klines.Kline)
 	closeChan := make(chan struct{})
 	errChan := make(chan error)
 
-	st := klines.NewStreamer("BTCUSDT", nil, errChan, closeChan)
+	st := klines.NewStreamer("BTCUSDT", klineChan, errChan, closeChan)
 	if err := st.Dial(); err != nil {
 		panic(err)
 	}
+
+	go func() {
+		for {
+			select {
+			case kline := <-klineChan:
+				log.Println(kline)
+			case err := <-errChan:
+				log.Fatal(err)
+			}
+		}
+	}()
 
 	<-shutdownCh
 	go func() {
